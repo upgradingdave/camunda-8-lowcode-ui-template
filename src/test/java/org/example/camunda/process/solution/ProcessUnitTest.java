5fdd8ecadd16dcd0499a4b000ecef99716e4e425
Dave Paroulek <david.paroulek@camunda.com>
2022-08-23 10:09:33 -0400

    accept java auto formatting changes

--- src/test/java/org/example/camunda/process/solution/ProcessUnitTest.java
+++ src/test/java/org/example/camunda/process/solution/ProcessUnitTest.java
@@ -1,11 +1,10 @@
 package org.example.camunda.process.solution;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
 import io.camunda.zeebe.client.ZeebeClient;
 import io.camunda.zeebe.process.test.api.ZeebeTestEngine;
 import io.camunda.zeebe.spring.test.ZeebeSpringTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 /**
  * @see
@@ -19,7 +18,7 @@ public class ProcessUnitTest {
 
   @Autowired private ZeebeTestEngine engine;
 
-  //@MockBean private MyService myService;
+  // @MockBean private MyService myService;
 
   /*
   @Test
